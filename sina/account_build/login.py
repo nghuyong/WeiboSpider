e329b94768f5b09543cbfd7a01efb9ac3e5eb880
huyong <nghuyong@163.com>
2018-12-25 16:05:51 +0800

    MOD: Update README.md

--- sina/account_build/login.py
+++ sina/account_build/login.py
@@ -1,17 +1,12 @@
 import os
-import time
-from io import BytesIO
 import pymongo
-from PIL import Image
 from pymongo.errors import DuplicateKeyError
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from os import listdir
 import sys
+
 sys.path.append(os.getcwd())
 from sina.settings import LOCAL_MONGO_HOST, LOCAL_MONGO_PORT, DB_NAME
 
@@ -22,7 +17,7 @@ class WeiboLogin():
     def __init__(self, username, password):
         os.system('pkill -f phantom')
         self.url = 'https://passport.weibo.cn/signin/login?entry=mweibo&r=https://weibo.cn/'
-        self.browser = webdriver.PhantomJS()
+        self.browser = webdriver.Chrome()
         self.browser.set_window_size(1050, 840)
         self.wait = WebDriverWait(self.browser, 20)
         self.username = username
@@ -41,141 +36,12 @@ class WeiboLogin():
         password.send_keys(self.password)
         submit.click()
 
-    def get_position(self):
-        """
-        获取验证码位置
-        :return: 验证码位置元组
-        """
-        try:
-            img = self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'patt-shadow')))
-        except TimeoutException:
-            print('未出现验证码')
-            self.open()
-        time.sleep(2)
-        location = img.location
-        size = img.size
-        top, bottom, left, right = location['y'], location['y'] + size['height'], location['x'], location['x'] + size[
-            'width']
-        return top, bottom, left, right
-
-    def get_screenshot(self):
-        """
-        获取网页截图
-        :return: 截图对象
-        """
-        screenshot = self.browser.get_screenshot_as_png()
-        screenshot = Image.open(BytesIO(screenshot))
-        return screenshot
-
-    def get_image(self, name='captcha.png'):
-        """
-        获取验证码图片
-        :return: 图片对象
-        """
-        top, bottom, left, right = self.get_position()
-        print('验证码位置', top, bottom, left, right)
-        screenshot = self.get_screenshot()
-        captcha = screenshot.crop((left, top, right, bottom))
-        captcha.save(name)
-        return captcha
-
-    def is_pixel_equal(self, image1, image2, x, y):
-        """
-        判断两个像素是否相同
-        :param image1: 图片1
-        :param image2: 图片2
-        :param x: 位置x
-        :param y: 位置y
-        :return: 像素是否相同
-        """
-        # 取两个图片的像素点
-        pixel1 = image1.load()[x, y]
-        pixel2 = image2.load()[x, y]
-        threshold = 20
-        if abs(pixel1[0] - pixel2[0]) < threshold and abs(pixel1[1] - pixel2[1]) < threshold and abs(
-                        pixel1[2] - pixel2[2]) < threshold:
-            return True
-        else:
-            return False
-
-    def same_image(self, image, template):
-        """
-        识别相似验证码
-        :param image: 待识别验证码
-        :param template: 模板
-        :return:
-        """
-        # 相似度阈值
-        threshold = 0.99
-        count = 0
-        for x in range(image.width):
-            for y in range(image.height):
-                # 判断像素是否相同
-                if self.is_pixel_equal(image, template, x, y):
-                    count += 1
-        result = float(count) / (image.width * image.height)
-        if result > threshold:
-            print('成功匹配')
-            return True
-        return False
-
-    def detect_image(self, image):
-        """
-        匹配图片
-        :param image: 图片
-        :return: 拖动顺序
-        """
-        for template_name in listdir(TEMPLATES_FOLDER):
-            template = Image.open(TEMPLATES_FOLDER + template_name)
-            if self.same_image(image, template):
-                # 返回顺序
-                numbers = [int(number) for number in list(template_name.split('.')[0])]
-                print('拖动顺序', numbers)
-                return numbers
-
-    def move(self, numbers):
-        """
-        根据顺序拖动
-        :param numbers:
-        :return:
-        """
-        # 获得四个按点
-        circles = self.browser.find_elements_by_css_selector('.patt-wrap .patt-circ')
-        dx = dy = 0
-        for index in range(4):
-            circle = circles[numbers[index] - 1]
-            # 如果是第一次循环
-            if index == 0:
-                # 点击第一个按点
-                ActionChains(self.browser) \
-                    .move_to_element_with_offset(circle, circle.size['width'] / 2, circle.size['height'] / 2) \
-                    .click_and_hold().perform()
-            else:
-                # 小幅移动次数
-                times = 30
-                # 拖动
-                for i in range(times):
-                    ActionChains(self.browser).move_by_offset(dx / times, dy / times).perform()
-                    time.sleep(1 / times)
-            # 如果是最后一次循环
-            if index == 3:
-                # 松开鼠标
-                ActionChains(self.browser).release().perform()
-            else:
-                # 计算下一次偏移
-                dx = circles[numbers[index + 1] - 1].location['x'] - circle.location['x']
-                dy = circles[numbers[index + 1] - 1].location['y'] - circle.location['y']
-
     def run(self):
         """
         破解入口
         :return:
         """
         self.open()
-        # 获取验证码图片
-        image = self.get_image('captcha.png')
-        numbers = self.detect_image(image)
-        self.move(numbers)
         WebDriverWait(self.browser, 30).until(
             EC.title_is('我的首页')
         )
