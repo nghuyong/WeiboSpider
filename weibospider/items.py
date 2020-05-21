8922a3425512b70362a28b2b405539699cbc1b11
huyong <nghuyong@163.com>
2020-05-21 09:34:41 +0800

    [FIX] fix 'weibo_id' of commentItem #ISSUE152

--- weibospider/items.py
+++ weibospider/items.py
@@ -56,7 +56,7 @@ class CommentItem(Item):
     _id = Field()
     comment_user_id = Field()  # 评论用户的id
     content = Field()  # 评论的内容
-    weibo_url = Field()  # 评论的微博的url
+    weibo_id = Field()  # 评论的微博的url
     created_at = Field()  # 评论发表时间
     like_num = Field()  # 点赞数
     crawl_time = Field()  # 抓取时间戳
