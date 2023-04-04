974f7a6a679099f44e41ad5743c35fe3f67d6871
liurundong <liurundong@stu.scau.edu.cn>
2023-04-05 03:16:22 +0800

    [ADD] add search hour by hour search keyword hour by hour from start time to end time.
    set search by time as default.
    
    set search result sort by time as default.
    
    TODO: develop search result sort by various kind.

--- weibospider/spiders/search.py
+++ weibospider/spiders/search.py
@@ -6,6 +6,7 @@ Created Time: 2022/10/22
 """
 import json
 import re
+from datetime import datetime, timedelta
 from scrapy import Spider, Request
 from spiders.common import parse_tweet_info, parse_long_tweet
 
@@ -21,45 +22,54 @@ class SearchSpider(Spider):
         """
         爬虫入口
         """
+
         # 这里keywords可替换成实际待采集的数据
-        keywords = ['丽江']
-        start_time = "2022-10-01-0"  # 格式为 年-月-日-小时, 2022-10-01-0 表示2022年10月1日0时
-        end_time = "2022-10-07-23"  # 格式为 年-月-日-小时, 2022-10-07-23 表示2022年10月7日23时
-        is_search_with_specific_time_scope = True  # 是否在指定的时间区间进行推文搜索
-        is_sort_by_hot = True  # 是否按照热度排序,默认按照时间排序
+        keywords = ['丽江']                 # 检索关键词
+        dt_parse_str = '%Y-%m-%d-%H'        # datetime format
+        start_time = "2023-04-01-00"
+        end_time = "2023-04-04-23"
+        # TODO set sort methods
+        # is_sort_by_hot = True               # 是否按照热度排序,默认按照时间排序
+
+        start_dt = datetime.strptime(start_time, dt_parse_str)
+        end_dt = datetime.strptime(end_time, dt_parse_str)
+        ahour_delta = timedelta(hours=1)
+
         for keyword in keywords:
-            if is_search_with_specific_time_scope:
-                url = f"https://s.weibo.com/weibo?q={keyword}&timescope=custom%3A{start_time}%3A{end_time}&page=1"
-            else:
-                url = f"https://s.weibo.com/weibo?q={keyword}&page=1"
-            if is_sort_by_hot:
-                url += "&xsort=hot"
-            yield Request(url, callback=self.parse, meta={'keyword': keyword})
+            dt = start_dt
+            while(dt < end_dt):
+                url = f"https://s.weibo.com/weibo?q={keyword}&timescope=custom%:{dt.strftime(dt_parse_str)}:{(dt + ahour_delta).strftime(dt_parse_str)}"
+                # if is_sort_by_hot:
+                #     url += "&xsort=hot"
+                yield Request(url, callback=self.parse, meta={'keyword': keyword})
+                dt += ahour_delta
 
     def parse(self, response, **kwargs):
         """
-        网页解析
+        检索结果页面解析
         """
+
         html = response.text
         tweet_ids = re.findall(r'weibo\.com/\d+/(.+?)\?refer_flag=1001030103_" ', html)
         for tweet_id in tweet_ids:
             url = f"https://weibo.com/ajax/statuses/show?id={tweet_id}"
-            yield Request(url, callback=self.parse_tweet, meta=response.meta)
+            yield Request(url, callback=self.parse_tweet, meta=response.meta, priority=9)
         next_page = re.search('<a href="(.*?)" class="next">下一页</a>', html)
         if next_page:
             url = "https://s.weibo.com" + next_page.group(1)
-            yield Request(url, callback=self.parse, meta=response.meta)
+            yield Request(url, callback=self.parse, meta=response.meta, priority=8)
 
     @staticmethod
     def parse_tweet(response):
         """
         解析推文
         """
+
         data = json.loads(response.text)
         item = parse_tweet_info(data)
         item['keyword'] = response.meta['keyword']
         if item['isLongText']:
             url = "https://weibo.com/ajax/statuses/longtext?id=" + item['mblogid']
-            yield Request(url, callback=parse_long_tweet, meta={'item': item})
+            yield Request(url, callback=parse_long_tweet, meta={'item': item}, priority=10)
         else:
             yield item
